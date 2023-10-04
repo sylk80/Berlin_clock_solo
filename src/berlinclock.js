61be321b61a9dc0a52978361a757a1e437230c4a
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-10-04 14:53:34 +0200

    Chore : refactor

--- src/berlinclock.js
+++ src/berlinclock.js
@@ -1,5 +1,11 @@
 class BerlinClock {}
 
-const show_berlin_clock = () => "Input time is required";
+const show_berlin_clock = (time) => {
+    let result = null
+    if(time === null || typeof time === 'undefined') {
+        result = "Input time is required"
+    }
+    return result;
+}
 
 module.exports = {BerlinClock, show_berlin_clock}
