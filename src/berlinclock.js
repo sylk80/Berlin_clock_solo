5e899e7f50bc7af2f04c40a462ea8e179e75f167
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-10-04 14:55:01 +0200

    Test: input length acceptance - GREEN

--- src/berlinclock.js
+++ src/berlinclock.js
@@ -4,6 +4,8 @@ const show_berlin_clock = (time) => {
     let result = null
     if(time === null || typeof time === 'undefined') {
         result = "Input time is required"
+    } else if(time.length > 8 || time.length < 8) {
+        result = "Time required in HH:mm:ss format"
     }
     return result;
 }
