5cb3953583d61cd9eb2651f341bce982b7805490
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-10-04 15:38:10 +0200

    Chore: refactor

--- src/berlinclock.js
+++ src/berlinclock.js
@@ -1,21 +1,30 @@
 class BerlinClock {
 
+    time = null
+
+    constructor(time) {
+        this.time = time
+    }
+
+    checkValidity() {
+        let error = null
+        if (this.time === null || typeof this.time === 'undefined') {
+            error = "Input time is required"
+        } else if (this.time.length > 8 || this.time.length < 8) {
+            error = "Time required in HH:mm:ss format"
+        }
+        return error
+    }
+
+    showClock() {
+        let error = this.checkValidity();
+        return  error === null ? "O\nRROO\nRROO\nYYRYYRYYRYY\nYOOO": error
+    }
 }
 
 const show_berlin_clock = (time) => {
-    let result = null
-    if(time === null || typeof time === 'undefined') {
-        result = "Input time is required"
-    } else if(time.length > 8 || time.length < 8) {
-        result = "Time required in HH:mm:ss format"
-    } else {
-        result = "O\n" +
-            "RROO\n" +
-            "RROO\n" +
-            "YYRYYRYYRYY\n" +
-            "YOOO"
-    }
-    return result;
+    let berlinClock = new BerlinClock(time)
+    return berlinClock.showClock();
 }
 
 module.exports = {BerlinClock, show_berlin_clock}
