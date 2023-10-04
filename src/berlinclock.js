d7744da686a86f9dba0e7896c011e022f418e632
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-10-04 14:50:41 +0200

    Test: input acceptance - GREEN

--- src/berlinclock.js
+++ src/berlinclock.js
@@ -1,5 +1,5 @@
 class BerlinClock {}
 
-const show_berlin_clock = () => "Time required in HH:mm:ss format";
+const show_berlin_clock = () => "Input time is required";
 
 module.exports = {BerlinClock, show_berlin_clock}
