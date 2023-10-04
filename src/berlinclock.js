691aabb98ccca469525167d12bf3f605267c2fc0
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-10-04 15:16:49 +0200

    Test: output characters acceptance - GREEN

--- src/berlinclock.js
+++ src/berlinclock.js
@@ -1,4 +1,6 @@
-class BerlinClock {}
+class BerlinClock {
+
+}
 
 const show_berlin_clock = (time) => {
     let result = null
@@ -6,6 +8,12 @@ const show_berlin_clock = (time) => {
         result = "Input time is required"
     } else if(time.length > 8 || time.length < 8) {
         result = "Time required in HH:mm:ss format"
+    } else {
+        result = "O\n" +
+            "RROO\n" +
+            "RROO\n" +
+            "YYRYYRYYRYY\n" +
+            "YOOO"
     }
     return result;
 }
