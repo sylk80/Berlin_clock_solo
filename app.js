77ac915d1d3e7d437243a122a57851192c514a8b
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-10-18 15:29:12 +0200

    Test: api post error message GREEN

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.post("/clock", function (req, res) {
-        res.status(400).json({"error":""})
+        res.status(400).json({"error":"Input time is required"})
 });
 
 module.exports = app
