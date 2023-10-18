4267b63ef42ba63cdac8ab3865161bea6ae3e05f
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-10-18 14:41:04 +0200

    Test: api test type RED

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,15 +1,18 @@
 
-const server = require('../index.js');
+const app = require('../app.js');
 const supertest = require('supertest');
-const requestWithSupertest = supertest(server);
+const requestWithSupertest = supertest(app);
 
 describe('Berlin clock Endpoints', () => {
 
-    it('GET /clock should return berloiclock', async () => {
+    it('GET /clock should return berlinclock', async () => {
+        const res = await requestWithSupertest.get('/clock');
+        expect(res.status).toEqual(200);
+    });
+    it('GET /clock should return json', async () => {
         const res = await requestWithSupertest.get('/clock');
         expect(res.status).toEqual(200);
         expect(res.type).toEqual(expect.stringContaining('json'));
-        expect(res.body).toHaveProperty('clock')
     });
 
 });
